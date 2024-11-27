c453c8e95dd6d29f73946122d52d5658bbab9a3c
Casper Engelmann <me@casperengelmann.com>
2024-11-27 15:11:50 +0100

    remove migrations

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20241125_230512 from './20241125_230512';
-
-export const migrations = [
-  {
-    up: migration_20241125_230512.up,
-    down: migration_20241125_230512.down,
-    name: '20241125_230512'
-  },
-];
